3a521c159bf607d302f81b2c52d43143ae254685
pan-1123 <100756312+pan-1123@users.noreply.github.com>
2023-06-14 17:23:33 +0800

    Update resnet_encoder.py

--- networks/resnet_encoder.py
+++ networks/resnet_encoder.py
@@ -13,32 +13,54 @@ import torch.nn as nn
 import torchvision.models as models
 import torch.utils.model_zoo as model_zoo
 
+def get_upsample_filter(size):
+    """Make a 2D bilinear kernel suitable for upsampling"""
+    factor = (size + 1) // 2
+    if size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:size, :size]
+    filter = (1 - abs(og[0] - center) / factor) * \
+             (1 - abs(og[1] - center) / factor)
+    return torch.from_numpy(filter).float()
 
 class ResNetMultiImageInput(models.ResNet):
     """Constructs a resnet model with varying number of input images.
     Adapted from https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+    Adapted from https://github.com/nianticlabs/monodepth2/edit/master/networks/resnet_encoder.py
     """
     def __init__(self, block, layers, num_classes=1000, num_input_images=1):
         super(ResNetMultiImageInput, self).__init__(block, layers)
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(
-            num_input_images * 3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # self.inplanes = 64
+        # self.conv1 = nn.Conv2d(
+        #    num_input_images * 3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        # self.bn1 = nn.BatchNorm2d(64)
+        # self.relu = nn.ReLU(inplace=True)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        in_ch, out_ch = 64,96
+        self.deconv1 = nn.ConvTranspose2d(in_channels=in_ch, out_channels=out_ch, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=in_ch, out_channels=out_ch, kernel_size=4, stride=2, padding=1)
+        
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-
+        # for m in self.modules():
+            # if isinstance(m, nn.Conv2d):
+                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            # elif isinstance(m, nn.BatchNorm2d):
+                # nn.init.constant_(m.weight, 1)
+                # nn.init.constant_(m.bias, 0)
+          for m in self.modules():
+              if isinstance(m, nn.ConvTranspose2d):
+                  c1, c2, h, w = m.weight.data.size()
+                  weight = get_upsample_filter(h)
+                  m.weight.data = weight.view(1, 1, h, w).repeat(c1, c2, 1, 1)
+                  if m.bias is not None:
+                      m.bias.data.zero_()     
+         
 def resnet_multiimage_input(num_layers, pretrained=False, num_input_images=1):
     """Constructs a ResNet model.
     Args:
@@ -84,13 +106,24 @@ class ResnetEncoder(nn.Module):
         if num_layers > 34:
             self.num_ch_enc[1:] *= 4
 
-    def forward(self, input_image):
+    # def forward(self, input_image):
+        # self.features = []
+        # x = (input_image - 0.45) / 0.225
+        # x = self.encoder.conv1(x)
+        # x = self.encoder.bn1(x)
+        # self.features.append(self.encoder.relu(x))
+        # self.features.append(self.encoder.layer1(self.encoder.maxpool(self.features[-1])))
+        
+    def forward(self) :
         self.features = []
-        x = (input_image - 0.45) / 0.225
-        x = self.encoder.conv1(x)
-        x = self.encoder.bn1(x)
-        self.features.append(self.encoder.relu(x))
-        self.features.append(self.encoder.layer1(self.encoder.maxpool(self.features[-1])))
+        dct_y  = torch.from_numpy(np.random.randn(16, 64, 28, 28)).float()
+        dc_cb = torch.from_numpy(np.random.randn(16, 64, 14, 14)).float()
+        dc_cr = torch.from_numpy(np.random.randn(16, 64, 14, 14)).float()
+        
+        dct_cb = self.deconv1(dc_cb)
+        dct_cr = self.deconv2(dc_cr)
+        x = torch.cat((dct_y, dct_cb, dct_cr), 1)   
+        self.features.append(self.encoder.layer1(x))
         self.features.append(self.encoder.layer2(self.features[-1]))
         self.features.append(self.encoder.layer3(self.features[-1]))
         self.features.append(self.encoder.layer4(self.features[-1]))
